9335ba66275aeb1ae500db18d3f67a7e85afcbde
Ignacio Bustos <ark.zikael@gmail.com>
2024-02-28 22:45:30 -0300

    commit

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -4,6 +4,11 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+  asdf{
+    lkgh asdriohgioulñzsdh fgluansbdklñfbg ñauksdfbnuikg bnasrduilg
+  }
   <React.StrictMode>
     
 
