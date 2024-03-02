d48ab93fc0df378a93d1f185775050a9a2d42b53
ErikSanzana <ersanzana@gmail.com>
2024-03-02 16:00:43 -0300

    carousel con json

--- front/src/components/Carousel.jsx
+++ front/src/components/Carousel.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import img1 from '../assets/img/jabon1.jpg';
-/* import img2 from '../assets/img/jabon2.jpg';
-import img3 from '../assets/img/jabon3.jpg';
-import img4 from '../assets/img/jabon4.jpg';
-import img5 from '../assets/img/jabon5.jpg';
-import img6 from '../assets/img/jabon6.jpg'; */
+import ProductCard from './ProductCard'; // Asegúrate de tener la ruta correcta
 
 function Carousel() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://finalbadrequest-default-rtdb.firebaseio.com/productos.json')
+      .then(response => {
+        setProducts(response.data); // Aquí seteamos los datos directamente
+      })
+      .catch(error => {
+        console.error('Error fetching products:', error);
+      });
+  }, []);
+
   const settings = {
     dots: true,
     infinite: true,
@@ -24,24 +32,11 @@ function Carousel() {
   return (
     <div className="carousel-container">
       <Slider {...settings}>
-        <div>
-          <img src={img1} alt="Imagen 1" className="carousel-image" />
-        </div>
-{/*         <div>
-          <img src={img2} alt="Imagen 2" className="carousel-image" />
-        </div>
-        <div>
-          <img src={img3} alt="Imagen 3" className="carousel-image" />
-        </div>
-        <div>
-          <img src={img4} alt="Imagen 4" className="carousel-image" />
-        </div>
-        <div>
-          <img src={img5} alt="Imagen 5" className="carousel-image" />
-        </div>
-        <div>
-          <img src={img6} alt="Imagen 6" className="carousel-image" />
-        </div> */}
+        {products.map(product => (
+          <div key={product.id}>
+            <ProductCard product={product} />
+          </div>
+        ))}
       </Slider>
     </div>
   );
