aa3554708da80acc8c2ab3fbec3de11b686d2b78
Gerardo <grd6@outlook.com>
2024-03-16 20:30:34 -0300

    update user profile

--- front/src/context/config/constant.js
+++ front/src/context/config/constant.js
@@ -1,10 +1,18 @@
 export const URLBASE = "https://backend-backup-3tm8.onrender.com";
 
-
-
-
 export const ENDPOINT = {
   login: URLBASE + `/api/v1/login/user/login`,
   registarUsuario: URLBASE + `/api/v1/user`,
   products: URLBASE + `/api/v1/store/products`,
+
+
+
+
+
+
+
+  
+
+  orderUser: URLBASE + `/api/v1/store/user/order`,
+  userData: URLBASE + `/api/v1/admin/users/:id`,
 };
--- front/src/views/Profiles/UserProfile.jsx
+++ front/src/views/Profiles/UserProfile.jsx
@@ -1,61 +1,94 @@
 import Button from "react-bootstrap/esm/Button.js";
 import RegisterForm from "./../../components/RegisterForm.jsx";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { ENDPOINT } from "../../context/config/constant.js";
 
 // que funcione aca va, que se vea historial de productos, que se vea el formulario para editar datos. y que se vena sus propuios datos
 
-
 const UserProfile = () => {
-//   const whereIam = () => {
-//     return true;
-//   };
+  const [history, setHistory] = useState([]);
+  const [userData, setUserData] = useState(null);
+  console.log(userData);
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Impob25fc25vd0BuaWdodHdhdGNoLmNvbSIsImlhdCI6MTcxMDYyODc3MCwiZXhwIjoxNzEwNjMyMzcwfQ.2cpMh2waAoh_Yowz4hQErjTxLRfKoVfzybz9JHZN82A";
+  const userId = "1700001";
 
-//   useEffect(() => {
-//     whereIam();
-//   });
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
 
-// post axios 
+  const fetchUserData = async () => {
+    try {
+      const response = await axios.get(
+        // `${ENDPOINT.adminUsers}/${userId}`
+        "https://backend-backup-3tm8.onrender.com/api/v1/admin/users/1700001",
+        config
+      ); // solicita para obtener los datos del usuario
+      setUserData(response.data.user); // Almacenar los datos del usuario en el estado
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
+  const fetchHistory = async () => {
+    try {
+      const response = await axios.get(ENDPOINT.orderUser);
+      setHistory(response.data.order_history);
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+  useEffect(() => {
+    fetchHistory();
+    fetchUserData();
+  }, []);
+
+  //   const whereIam = () => {
+  //     return true;
+  //   };
+
+  //   useEffect(() => {
+  //     whereIam();
+  //   });
+
+  // post axios
 
   return (
     <>
       <section className="mainAdmin">
-       
         <article className="dataView">
           {/* ver sus propios datos, con un get  user su id map pa los datos */}
-          <ul>
-            <li>a</li>
-            <li>s</li>
-            <li>d</li>
-            <li>4</li>
-            <li>g</li>
-            <li>f</li>
-          </ul>
+        
+          {userData && (
+            <ul>
+              <h6>Datos del usuario:</h6>
+              <li>RUT: {userData.rut}</li>
+              <li>Nombre: {userData.name}</li>
+              <li>Apellido: {userData.last_name}</li>
+              <li>Email: {userData.email}</li>
+            </ul>
+          )}
         </article>
 
         <article className="dataView">
-          
-          navigate to = favorites o ir a favoritos (boton navigate)
-        
-        
+          {/* navigate to = favorites o ir a favoritos (boton navigate) */}
         </article>
 
         <article className="dataView">
-          
           <div className="ListOfOrders">
             {/* usar axios y crear una lista con un map   get de buy_order y un map  */}
+            <h6>Historial</h6>
             <ul>
-              <li>a</li>
-              <li>s</li>
-              <li>d</li>
-              <li>4</li>
-              <li>g</li>
-              <li>f</li>
+              {history.map((order) => (
+                <li key={order.id}>
+                  {/* Muestrar detalles del historial de productos */}
+                  {order.product_code} - ${order.total_price}
+                </li>
+              ))}
             </ul>
-
           </div>
         </article>
       </section>
-
-
     </>
   );
 };
